17b4a905dcdf78b6ddbad7f7f23f92877d18945d
朱立焜 <zcillcoder@gmail.com>
2016-03-21 15:00:35 +0800

    add UIView category, can simply set view frame

---
+++ ZCCategory/UIView+ZCFrame.h
@@ -0,0 +1,22 @@
+//
+//  UIView+ZCFrame.h
+//  testPopView
+//
+//  Created by 朱立焜 on 16/3/21.
+//  Copyright © 2016年 朱立焜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ZCFrame)
+
+@property(nonatomic,assign)CGFloat x;
+@property(nonatomic,assign)CGFloat y;
+@property(nonatomic,assign)CGFloat centerX;
+@property(nonatomic,assign)CGFloat centerY;
+@property(nonatomic,assign)CGFloat width;
+@property(nonatomic,assign)CGFloat height;
+@property(nonatomic,assign)CGSize size;
+@property(nonatomic,assign)CGPoint origin;
+
+@end
